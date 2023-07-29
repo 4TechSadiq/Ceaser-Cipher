0c1816b7bea47a77e8df90b49d6b62edc542fd40
Mohammed Sadiq Ali <119723238+S4DIQ84@users.noreply.github.com>
2023-07-30 01:19:19 +0530

    Update main.py
    uploaded code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import random
+def caesar(direction,text,shift):
+  caesor_out = ""
+  if direction == "encode":
+    for letter in text:
+      text_pos = alphabet.index(letter)
+      new_pos = text_pos + shift
+      caesor_out+=alphabet[new_pos]
+    print(f"your encoded message is {caesor_out}")
+  elif direction == "decode":
+    for txt in text:
+      pos = alphabet.index(txt)
+      new_pos = pos - shift
+      caesor_out+= alphabet[new_pos]
+    print(f"decode msg is {caesor_out}")
+
+caesar(direction,text,shift)
