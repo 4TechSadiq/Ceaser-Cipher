7578952041d3466dd5a5894bf8a288b31f63bcb5
Mohammed Sadiq Ali <119723238+S4DIQ84@users.noreply.github.com>
2023-07-30 12:20:24 +0530

    Update main.py
    updated

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-def caesar(direction,text,shift):
-  caesor_out = ""
-  if direction == "encode":
-    for letter in text:
-      text_pos = alphabet.index(letter)
-      new_pos = text_pos + shift
-      caesor_out+=alphabet[new_pos]
-    print(f"your encoded message is {caesor_out}")
-  elif direction == "decode":
-    for txt in text:
-      pos = alphabet.index(txt)
-      new_pos = pos - shift
-      caesor_out+= alphabet[new_pos]
-    print(f"decode msg is {caesor_out}")
-
-caesar(direction,text,shift)
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+from art import logo
+print(logo)
+restart = True
+while restart:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  again = input("do you want to do again : yes / no : ")
+  if again == "no":
+    restart = False
+    print("Good Bie")
+    
